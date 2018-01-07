9d82be898aecdf99dfe2bb5b7d687b978bd44a27
Alexey Gavrilov <agavrilov@telenordigital.com>
2018-01-07 20:19:37 +0100

    Update travis.yml 2

--- expectit-core/src/test/java/net/sf/expectit/SshExample.java
+++ expectit-core/src/test/java/net/sf/expectit/SshExample.java
@@ -30,6 +30,7 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import java.io.IOException;
+import java.nio.channels.Pipe;
 import java.util.Properties;
 
 /**
